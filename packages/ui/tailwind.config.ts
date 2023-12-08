f146630969f8cbb630e6eb13e344b1d41c066a20
Elias Gabriel <eliasgabrielcf@gmail.com>
2023-12-08 14:34:54 -0300

    fix

--- packages/ui/tailwind.config.ts
+++ packages/ui/tailwind.config.ts
@@ -1,36 +1,38 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  darkMode: ["class"],
-  content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
-	],
-  theme: {
-    container: {
-      center: true,
-      padding: "2rem",
-      screens: {
-        "2xl": "1400px",
-      },
-    },
-    extend: {
-      keyframes: {
-        "accordion-down": {
-          from: { height: 0 },
-          to: { height: "var(--radix-accordion-content-height)" },
-        },
-        "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: 0 },
+import type { Config } from "tailwindcss";
+import sharedConfig from "@repo/tailwind-config";
+import tailwindAnimate from 'tailwindcss-animate';
+
+const config: Pick<Config, "prefix" | "presets" | "content"> = {
+  content: ["./src/**/*.tsx"],
+  presets: [sharedConfig, {
+    darkMode: ["class"],
+    theme: {
+      container: {
+        center: true,
+        padding: "2rem",
+        screens: {
+          "2xl": "1400px",
         },
       },
-      animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
+      extend: {
+        keyframes: {
+          "accordion-down": {
+            from: { height: 0 },
+            to: { height: "var(--radix-accordion-content-height)" },
+          },
+          "accordion-up": {
+            from: { height: "var(--radix-accordion-content-height)" },
+            to: { height: 0 },
+          },
+        },
+        animation: {
+          "accordion-down": "accordion-down 0.2s ease-out",
+          "accordion-up": "accordion-up 0.2s ease-out",
+        },
       },
     },
-  },
-  plugins: [require("tailwindcss-animate")],
-}
+    plugins: [tailwindAnimate],
+  }],
+};
+
+export default config;
