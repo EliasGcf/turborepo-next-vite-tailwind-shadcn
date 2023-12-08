1a0da098d6a01b0df73367713686bb4cb0be64c7
Elias Gabriel <eliasgabrielcf@gmail.com>
2023-12-08 13:06:50 -0300

    working

--- apps/docs/tailwind.config.ts
+++ apps/docs/tailwind.config.ts
@@ -4,7 +4,7 @@ import type { Config } from "tailwindcss";
 import sharedConfig from "@repo/tailwind-config";
 
 const config: Pick<Config, "content" | "presets"> = {
-  content: ["./src/app/**/*.tsx"],
+  content: ["./src/app/**/*.tsx", "../../packages/ui/**/*.tsx"],
   presets: [sharedConfig],
 };
 
---
+++ apps/web/src/App.tsx
@@ -0,0 +1,10 @@
+import { Card } from "@repo/ui/card";
+
+export function App() {
+  return (
+    <div className="">
+      <h1 className="text-red-600">hello vite asdf</h1>
+    </div>
+  );
+}
+
